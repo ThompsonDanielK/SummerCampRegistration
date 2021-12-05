651c6e12ae01215054179f6af4790d21ecde1a46
Eric Rausch <erausch32@gmail.com>
2021-12-05 11:38:46 -0500

    Added tables to .sql & CAMP.vpd

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -30,3 +30,69 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg4
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 GO
 
+CREATE TABLE family (
+	family_id int IDENTITY(100001, 1) PRIMARY KEY,
+	parent_guardian_name nvarchar(100) NOT NULL,
+	address nvarchar(200),
+	city nvarchar(100),
+	state nvarchar(2),
+	zip nvarchar(10),
+	phone nvarchar(12)
+)
+
+CREATE TABLE campers (
+	camper_code int IDENTITY(200001, 1) PRIMARY KEY,
+	family_id int NOT NULL,
+	first_name nvarchar(50) NOT NULL,
+	last_name nvarchar(50) NOT NULL,
+	dob date NOT NULL,
+	medications nvarchar(200) DEFAULT 'None',
+	allergies nvarchar(200) DEFAULT 'None', 
+	special_needs nvarchar(1000) DEFAULT 'None'
+)
+
+CREATE TABLE updates (
+	request_id int IDENTITY(300001, 1),
+	field_to_be_changed nvarchar(50),
+	camper_code int NOT NULL,
+	action nvarchar(20) NOT NULL,
+	new_data nvarchar(1000) NOT NULL,
+	old_data nvarchar(1000),
+	requestor nvarchar(100) NOT NULL,
+	status nvarchar(20) NOT NULL
+	CONSTRAINT PK_updates PRIMARY KEY (request_id, field_to_be_changed)
+);
+
+GO
+
+SET IDENTITY_INSERT family ON;
+INSERT INTO family (family_id, parent_guardian_name, address, city, state, zip, phone)
+	VALUES (100001, 'Mary Andrews', '100 First St.', 'Columbus', 'OH', '43111', '614-222-3333');
+INSERT INTO family (family_id, parent_guardian_name, address, city, state, zip, phone)
+	VALUES (100002, 'Mike Bowers', '200 Second Ave.', 'Dublin', 'OH', '43222', '614-444-5555');
+INSERT INTO family (family_id, parent_guardian_name, address, city, state, zip, phone)
+	VALUES (100003, 'Molly Carter', '300 Third St.', 'Hilliard', 'OH', '43333', '614-666-7777');
+SET IDENTITY_INSERT family OFF;
+
+GO
+
+SET IDENTITY_INSERT campers ON;
+INSERT INTO campers (camper_code, family_id, first_name, last_name, dob)
+	VALUES(200001, 100001, 'Alex', 'Andrews', '2010-01-01');
+INSERT INTO campers (camper_code, family_id, first_name, last_name, dob)
+	VALUES(200002, 100001, 'Brandy', 'Andrews', '2009-02-03');
+INSERT INTO campers (camper_code, family_id, first_name, last_name, dob)
+	VALUES(200003, 100002, 'Cathy', 'Bowers', '2011-03-03');
+INSERT INTO campers (camper_code, family_id, first_name, last_name, dob)
+	VALUES(200004, 100003, 'David', 'Carter', '2010-04-04');
+INSERT INTO campers (camper_code, family_id, first_name, last_name, dob)
+	VALUES(200005, 100003, 'Erica', 'Carter', '2009-05-05');
+SET IDENTITY_INSERT campers OFF;
+
+
+
+ALTER TABLE campers ADD FOREIGN KEY (family_id) REFERENCES family(family_id);
+
+ALTER TABLE updates ADD FOREIGN KEY (camper_code) REFERENCES campers(camper_code);
+ALTER TABLE updates ADD	CONSTRAINT Chk_action CHECK (action IN ('ADD', 'Update', 'Delete'));
+ALTER TABLE updates ADD CONSTRAINT Chk_status CHECK (status IN ('Pending', 'Updated', 'Rejected'));
