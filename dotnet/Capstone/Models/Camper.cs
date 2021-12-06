bd7e0e4addf2f64e34b6bbd43c895185ee71cd1e
Daniel Thompson <kritterdoggo@gmail.com>
2021-12-05 22:52:21 -0500

    Finished Camper Model and added Family & Update Model

--- dotnet/Capstone/Models/Camper.cs
+++ dotnet/Capstone/Models/Camper.cs
@@ -7,7 +7,21 @@ namespace Capstone.Models
 {
     public class Camper
     {
-        public int Id { get; set; }
+        public int CamperCode { get; set; }
+
+        public int FamilyId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public List<string> Medications { get; set; }
+
+        public List<string> Allergies { get; set; }
+
+        public List<string> SpecialNeeds { get; set; }
 
         public Camper()
         {
---
+++ dotnet/Capstone/Models/Update.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Update
+    {
+        public int RequestId { get; set; }
+
+        public string FieldToBeChanged { get; set; }
+
+        public int CamperCode { get; set; }
+
+        public Update()
+        {
+
+        }
+    }
+}
