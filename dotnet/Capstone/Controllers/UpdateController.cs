b37fe86b27d672ff2451c7a955a9758b6dd136a0
Eric Rausch <erausch32@gmail.com>
2021-12-09 00:53:08 -0500

    Moved update controller methods to update controller

--- dotnet/Capstone/Controllers/UpdateController.cs
+++ dotnet/Capstone/Controllers/UpdateController.cs
@@ -1,4 +1,5 @@
 ﻿using Capstone.Models;
+using Capstone.DAO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,50 @@ using System.Threading.Tasks;
 
 namespace Capstone.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
     public class UpdateController : ControllerBase
     {
-        //public ActionResult PostFamilyUpdateRequest(Family updatedFamily)
-        //{
-        //    return Ok();
-        //}
+        private readonly IUpdatesDao updates;
+        private readonly ICampDao camp;
+        public UpdateController(ICampDao camp, IUpdatesDao updates)
+        {
+            this.camp = camp;
+            this.updates = updates;
+        }
+
+        [HttpPut("camper")]
+        public ActionResult UpdateCamper(Camper camper)
+        {
+            int userId = int.Parse(this.User.FindFirst("sub").Value);
+            Camper oldCamperData = camp.FetchCamper(camper.CamperCode);
+
+            int requestId = updates.AddNewCamperUpdateRequest(userId, camper, oldCamperData);
+            if (this.User.IsInRole("admin")) updates.ProcessApprovedRequests(requestId);
+
+            return Ok();
+
+        }
+
+        [HttpPut("family")]
+        public ActionResult UpdateFamily(Family family)
+        {
+            int userId = int.Parse(this.User.FindFirst("sub").Value);
+            Family oldFamilyData = camp.FetchFamily(family.FamilyId);
+
+            int requestId = updates.AddNewFamilyUpdateRequest(userId, family, oldFamilyData);
+            if (this.User.IsInRole("admin")) updates.ProcessApprovedRequests(requestId);
+
+            return Ok();
+
+        }
+
+        [HttpPut("approval/{requestId}")]
+        public ActionResult UpdateRequestApproved(int requestId)
+        {
+            if (this.User.IsInRole("admin")) updates.ProcessApprovedRequests(requestId);
+            return Ok();
+        }
     }
 }
